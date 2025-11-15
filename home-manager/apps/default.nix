48a3af59022fa8cfd7b76ed785a8cb080e84df7d
depaysement-jikan <depaysement-jikan@gmail.com>
2025-11-15 10:28:29 -0600

    hyprland config

--- home-manager/apps/default.nix
+++ home-manager/apps/default.nix
@@ -1,14 +1,16 @@
 { lib, config, ... }:
 let cfg = config.myHomeConfig.apps;
 in {
-  imports = [ ./browsers ];
+  imports = [ ./browsers ./web ];
 
   options.myHomeConfig.apps = {
     enable = lib.mkEnableOption "applications and GUI programs";
     browsers.enable = lib.mkEnableOption "web browsers";
+    web.enable = lib.mkEnableOption "web apps";
   };
 
   config = lib.mkIf cfg.enable {
     browsers.enable = lib.mkDefault cfg.browsers.enable;
+    web.enable = lib.mkDefault cfg.web.enable;
   };
 }
