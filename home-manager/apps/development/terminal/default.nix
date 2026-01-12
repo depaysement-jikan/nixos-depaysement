9d51d9df6267f54a9b137cc74e356b3d54d84c50
Yoshua Carrera Vargas <yoshua.carrera24@gmail.com>
2026-01-12 16:14:14 -0600

    add zsh config and tmux config

--- home-manager/apps/development/terminal/default.nix
+++ home-manager/apps/development/terminal/default.nix
@@ -1,8 +1,10 @@
 { lib, config, ... }: {
-  imports = [ ./yazi ];
+  imports = [ ./yazi ./zsh ./tmux ];
 
   options = { terminal.enable = lib.mkEnableOption "Enable terminal module"; };
   config = lib.mkIf config.myHomeConfig.apps.development.terminal.enable {
     yazi.enable = lib.mkDefault true;
+    zsh.enable = lib.mkDefault true;
+    tmux.enable = lib.mkDefault true;
   };
 }
