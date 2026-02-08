813e6bf968864fdfa36ecb072141ddb44382bfce
depaysement-jikan <depaysement-jikan@gmail.com>
2026-02-08 17:38:29 -0600

    adding more programs to config

---
+++ home-manager/apps/social/discord/default.nix
@@ -0,0 +1,6 @@
+{ pkgs, lib, config, ... }: {
+  options = { discord.enable = lib.mkEnableOption "Enable discord module"; };
+  config = lib.mkIf config.myHomeConfig.apps.social.discord.enable {
+    home.packages = with pkgs; [ discord ];
+  };
+}
