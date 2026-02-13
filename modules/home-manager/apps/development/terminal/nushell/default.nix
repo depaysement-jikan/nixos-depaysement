8f7668175a925cc144c3331c4b6e612b925dd31e
Yoshua Carrera Vargas <yoshua.carrera24@gmail.com>
2026-02-12 20:17:07 -0600

    zoxide on nushell

--- modules/home-manager/apps/development/terminal/nushell/default.nix
+++ modules/home-manager/apps/development/terminal/nushell/default.nix
@@ -10,8 +10,76 @@
       packages = with pkgs; [
         nufmt
         inshellisense
-        nushell
+        zoxide
       ];
+      file.".zoxide.nu" = {
+        source =
+          pkgs.runCommand "generate-zoxide-nu" {
+            buildInputs = [pkgs.zoxide];
+          } ''
+            zoxide init nushell > $out
+          '';
+      };
+    };
+    programs = {
+      nushell = {
+        enable = true;
+        configFile.text = ''
+          let carapace_completer = {|spans|
+              ${config.programs.carapace.package}/bin/carapace $spans.0 nushell ...$spans | from json
+          }
+
+          $env.config = {
+            ls: {
+              clickable_links: true
+            }
+
+            show_banner: false
+            edit_mode: "vi"
+
+            completions: {
+              algorithm: "fuzzy"
+              case_sensitive: false
+              quick: true
+              partial: true
+              sort: "smart"
+              external: {
+                enable: true
+                max_results: 100
+                completer: $carapace_completer
+              }
+            }
+            cursor_shape: {
+              vi_insert: line
+              vi_normal: block
+            }
+            keybindings: [
+              {
+                name: completion_menu
+                modifier: none
+                keycode: tab
+                mode: [vi_insert vi_normal]
+                event: { send: menu name: completion_menu }
+              },
+            ]
+            use_kitty_protocol: true
+            render_right_prompt_on_last_line: false
+          }
+
+          source ~/.zoxide.nu
+        '';
+        extraEnv = ''
+          $env.TRANSIENT_PROMPT_COMMAND = ">"
+          $env.CARAPACE_BRIDGES = 'zsh,fish,bash,inshellisense'
+        '';
+        shellAliases = {
+          nv = "nvim";
+          lg = "lazygit";
+          cd = "z";
+          git-co = "sh ~/.config/scripts/fuzzy-co.sh";
+        };
+      };
+      carapace.enable = true;
     };
   };
 }
