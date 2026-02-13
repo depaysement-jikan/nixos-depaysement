d294233bf2cf12edbcd81838728c53a5272db801
depaysement-jikan <depaysement-jikan@gmail.com>
2026-02-13 14:36:48 -0600

    adding pg, will clean up later

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -117,7 +117,7 @@
   };
 
   # Add stuff for your user as you see fit:
-  home.packages = with pkgs; [wget];
+  home.packages = with pkgs; [wget postgresql];
 
   # Enable home-manager and git
   programs = {
