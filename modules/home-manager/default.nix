b7333591c9895c545f689469ed8ca1a1d52e44c7
Yoshua Carrera Vargas <yoshua.carrera24@gmail.com>
2026-02-15 17:16:29 -0600

    final changes to waybar to make it a pill

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -121,7 +121,7 @@
   };
 
   # Add stuff for your user as you see fit:
-  home.packages = with pkgs; [wget swaynotificationcenter];
+  home.packages = with pkgs; [wget swaynotificationcenter cbonsai];
   services.swaync.enable = true;
 
   # Enable home-manager and git
