b4a162a4c9a92df7bb48927856707dde8800accf
Yoshua Carrera Vargas <yoshua.carrera24@gmail.com>
2026-02-16 17:05:18 -0600

    rclone setup

---
+++ modules/homelab/rclone/default.nix
@@ -0,0 +1,19 @@
+{
+  config,
+  pkgs,
+  ...
+}: {
+  environment.systemPackages = with pkgs; [rclone];
+  sops.templates.rclone = {
+    content = ''
+      [Garage]
+      type = s3
+      provider = Cloudflare
+      access_key_id = ${config.sops.placeholder.fluxAccessKeyId}
+      secret_access_key = ${config.sops.placeholder.fluxSecretKey}
+      region = auto
+      endpoint = https://${config.sops.placeholder.fluxEndpoint}
+    '';
+    path = "/etc/rclone.conf";
+  };
+}
