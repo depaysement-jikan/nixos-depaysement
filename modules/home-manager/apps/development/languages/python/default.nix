a52c4f0d987948469236bcedbd494400c9870e09
Yoshua Carrera Vargas <yoshua.carrera24@gmail.com>
2026-02-20 21:20:58 -0600

    lang tweaks

--- modules/home-manager/apps/development/languages/python/default.nix
+++ modules/home-manager/apps/development/languages/python/default.nix
@@ -6,6 +6,6 @@
 }: {
   options = {python.enable = lib.mkEnableOption "Enable python module";};
   config = lib.mkIf config.myHomeConfig.apps.development.languages.python.enable {
-    home.packages = with pkgs; [python3];
+    home.packages = with pkgs; [python3 uv pyright];
   };
 }
