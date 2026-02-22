2ad805aa6c56d4d9fd66b0dd9e58f4233fbc8a05
depaysement-jikan <depaysement-jikan@gmail.com>
2026-02-22 09:58:50 -0600

    built in helm autodeploy

--- modules/homelab/vaultwarden/default.nix
+++ modules/homelab/vaultwarden/default.nix
@@ -35,12 +35,10 @@
     #   kubectl delete job -n kube-system helm-install-vaultwarden
     #   kubectl delete pod -n kube-system helm-install-vaultwarden-bj2n6
     autoDeployCharts.vaultwarden = {
-      package = pkgs.lib.downloadHelmChart {
-        repo = "https://guerzon.github.io/vaultwarden";
-        chart = "vaultwarden";
-        version = "0.34.4";
-        chartHash = "sha256-qn2kfuXoLqHLyacYrBwvKgVb+qZjMu+E16dq9jJS3RE=";
-      };
+      name = "vaultwarden";
+      repo = "https://guerzon.github.io/vaultwarden";
+      version = "0.34.4";
+      hash = "sha256-qn2kfuXoLqHLyacYrBwvKgVb+qZjMu+E16dq9jJS3RE=";
       targetNamespace = "vaultwarden";
 
       values = {
