17cc4e6ceeea504f47fac0c4c221b01d6260712f
depaysement-jikan <depaysement-jikan@gmail.com>
2026-02-26 16:12:45 -0600

    correct grafana host

--- modules/homelab/grafana/default.nix
+++ modules/homelab/grafana/default.nix
@@ -5,7 +5,7 @@
       enable = lib.mkEnableOption "grafana";
       ingresshost = lib.mkOption {
         type = lib.types.str;
-        default = "prometheus.home";
+        default = "grafana.home";
       };
     };
   };
