5a578c89edeeb9e61c8c4a6756402d77a9451af0
depaysement-jikan <depaysement-jikan@gmail.com>
2026-02-27 23:50:42 -0600

    add disko back in and pin by uuid

--- hosts/tsukinara/disko/default.nix
+++ hosts/tsukinara/disko/default.nix
@@ -1,4 +1,5 @@
-{...}: {
+# /etc/nixos/disko-configuration.nix
+{
   disko = {
     devices = {
       disk.main = {
@@ -11,6 +12,12 @@
               name = "boot";
               size = "1M";
               type = "EF02";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                device = "/dev/disk/by-uuid/4D6F-33A5";
+              };
             };
             esp = {
               name = "ESP";
@@ -20,6 +27,7 @@
                 type = "filesystem";
                 format = "vfat";
                 mountpoint = "/boot";
+                device = "/dev/disk/by-uuid/4D6F-33A5";
               };
             };
             swap = {
@@ -27,6 +35,7 @@
               size = "4G";
               content = {
                 type = "swap";
+                device = "/dev/disk/by-uuid/b78dabd8-69ed-48e1-8356-3efb0cf8dc7c";
                 resumeDevice = true;
               };
             };
@@ -37,6 +46,7 @@
                 type = "filesystem";
                 format = "ext4";
                 mountpoint = "/";
+                device = "/dev/disk/by-uuid/2315fd0e-1171-4960-a8da-ad651e0c3f9c";
               };
             };
           };
