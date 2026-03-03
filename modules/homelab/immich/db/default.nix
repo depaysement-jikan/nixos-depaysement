4479af6e48799a02ca9885efc88ec5e395d2b3e9
Yoshua Carrera Vargas <yoshua.carrera24@gmail.com>
2026-03-02 21:26:23 -0600

    adding immich db

--- modules/homelab/immich/db/default.nix
+++ modules/homelab/immich/db/default.nix
@@ -4,25 +4,33 @@
   ...
 }: {
   config = lib.mkIf config.homelab.immich.enable {
-    services.k3s = {
-      manifests.immich-databases.content = {
-        apiVersion = "postgresql.cnpg.io/v1";
-        kind = "Cluster";
-        metadata = {
-          name = "immich-database";
-          namespace = "immich";
+    services.k3s.manifests.immich-databases.content = {
+      apiVersion = "postgresql.cnpg.io/v1";
+      kind = "Cluster";
+      metadata = {
+        name = "immich-database";
+        namespace = "immich";
+      };
+      spec = {
+        instances = config.homelab.immich.db.instances;
+
+        storage = {
+          size = config.homelab.immich.db.size;
         };
-        spec = {
-          instances = config.homelab.immich.db.instances;
-          storage = {
-            size = config.homelab.immich.db.size;
+
+        imageName = "ghcr.io/tensorchord/cloudnative-vectorchord:16.9-0.4.3";
+
+        postgresql = {
+          shared_preload_libraries = ["vchord.so"];
+        };
+
+        bootstrap = {
+          initdb = {
+            postInitApplicationSQL = [
+              "CREATE EXTENSION IF NOT EXISTS vchord CASCADE;"
+              "CREATE EXTENSION IF NOT EXISTS earthdistance CASCADE;"
+            ];
           };
-          imageName = "ghcr.io/tensorchord/cloudnative-vectorchord:16.9-0.4.3";
-          postgresql.shared_preload_libraries = ["vchord.so"];
-          pbootstrap.initdb.postInitApplicationSQL = [
-            "CREATE EXTENSION vchord CASCADE"
-            "CREATE EXTENSION earthdistance CASCADE"
-          ];
         };
       };
     };
