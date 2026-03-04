8e334764b30ec14f8f1c22078d540dc5401f6076
depaysement-jikan <depaysement-jikan@gmail.com>
2026-03-04 13:56:42 -0600

    adding enable options for flux

--- modules/homelab/flux/bucket/default.nix
+++ modules/homelab/flux/bucket/default.nix
@@ -1,23 +1,29 @@
-{config, ...}: let
+{
+  config,
+  lib,
+  ...
+}: let
   cfg = config.homelab.flux;
 in {
-  services.k3s = {
-    secrets = [
-      {
-        apiVersion = "source.toolkit.fluxcd.io/v1";
-        kind = "Bucket";
-        metadata = {
-          name = "flux-bucket";
-          namespace = "flux-system";
-        };
-        spec = {
-          interval = "1m";
-          bucketName = cfg.bucketName;
-          endpoint = cfg.endpoint;
-          provider = "aws";
-          secretRef.name = "flux-s3-credentials";
-        };
-      }
-    ];
+  config = lib.mkIf config.homelab.flux.enable {
+    services.k3s = {
+      secrets = [
+        {
+          apiVersion = "source.toolkit.fluxcd.io/v1";
+          kind = "Bucket";
+          metadata = {
+            name = "flux-bucket";
+            namespace = "flux-system";
+          };
+          spec = {
+            interval = "1m";
+            bucketName = cfg.bucketName;
+            endpoint = cfg.endpoint;
+            provider = "aws";
+            secretRef.name = "flux-s3-credentials";
+          };
+        }
+      ];
+    };
   };
 }
