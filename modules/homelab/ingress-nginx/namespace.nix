476a0071ced03c2801eccc7ce19dfbf36a1f35f1
Yoshua Carrera Vargas <yoshua.carrera24@gmail.com>
2026-03-04 14:22:12 -0600

    ingress enable flag + removing deprecated manifests

--- modules/homelab/ingress-nginx/namespace.nix
+++ modules/homelab/ingress-nginx/namespace.nix
@@ -1,15 +1,21 @@
-_: {
-  services.k3s.manifests.ingress-nginx-namespace.content = [
-    {
-      apiVersion = "v1";
-      kind = "Namespace";
-      metadata = {
-        labels = {
-          "app.kubernetes.io/instance" = "ingress-nginx";
-          "app.kubernetes.io/name" = "ingress-nginx";
+{
+  lib,
+  config,
+  ...
+}: {
+  config = lib.mkIf config.homelab.ingress.enable {
+    services.k3s.manifests.ingress-nginx-namespace.content = [
+      {
+        apiVersion = "v1";
+        kind = "Namespace";
+        metadata = {
+          labels = {
+            "app.kubernetes.io/instance" = "ingress-nginx";
+            "app.kubernetes.io/name" = "ingress-nginx";
+          };
+          name = "ingress-nginx";
         };
-        name = "ingress-nginx";
-      };
-    }
-  ];
+      }
+    ];
+  };
 }
