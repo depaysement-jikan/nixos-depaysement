705115e3c02a59b524a852ac3dff9ea29478fb1e
Yoshua Carrera Vargas <yoshua.carrera24@gmail.com>
2026-03-17 21:02:32 -0500

    python formatter

--- modules/home-manager/apps/development/languages/python/default.nix
+++ modules/home-manager/apps/development/languages/python/default.nix
@@ -6,6 +6,6 @@
 }: {
   options = {python.enable = lib.mkEnableOption "Enable python module";};
   config = lib.mkIf config.homeManager.apps.development.languages.python.enable {
-    home.packages = with pkgs; [python3 uv pyright];
+    home.packages = with pkgs; [python3 uv pyright black];
   };
 }
