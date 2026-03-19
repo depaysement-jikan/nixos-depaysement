1b8e6d1dc34256180b1f507f09a59c685ef68823
depaysement-jikan <depaysement-jikan@gmail.com>
2026-03-19 18:59:54 -0500

    clean up comments on home manager

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -1,12 +1,9 @@
-# This is your home-manager configuration file
-# Use this to configure your home environment (it replaces ~/.config/nixpkgs/home.nix)
 {
   outputs,
   pkgs,
   config,
   ...
 }: {
-  # You can import other home-manager modules here
   home = {
     stateVersion = "25.11";
     sessionPath = ["$HOME/.local/bin"];
@@ -16,24 +13,11 @@
 
   nixpkgs = {
     overlays = [
-      # Add overlays your own flake exports (from overlays and pkgs dir):
       outputs.overlays.additions
       outputs.overlays.modifications
       outputs.overlays.unstable-packages
-
-      # You can also add overlays exported from other flakes:
-      # neovim-nightly-overlay.overlays.default
-
-      # Or define it inline, for example:
-      # (final: prev: {
-      #   hi = final.hello.overrideAttrs (oldAttrs: {
-      #     patches = [ ./change-hello-to-hi.patch ];
-      #   });
-      # })
     ];
-    # Configure your nixpkgs instance
     config = {
-      # Disable if you don't want unfree packages
       allowUnfree = true;
     };
   };
@@ -43,11 +27,9 @@
     homeDirectory = "/home/depaysement";
   };
 
-  # Add stuff for your user as you see fit:
   home.packages = with pkgs; [wget swaynotificationcenter cbonsai lolcat fastfetch];
   services.swaync.enable = true;
 
-  # Enable home-manager and git
   programs = {
     wlogout.enable = true;
     home-manager.enable = config.homeManager.enable;
@@ -61,7 +43,6 @@
 
   xdg.configFile."git/config".force = true;
 
-  # Nicely reload system units when changing configs
   systemd.user.startServices = "sd-switch";
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
