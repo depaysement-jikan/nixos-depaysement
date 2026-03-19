b9d64474c641a770162953e057b09cb2408c2a43
depaysement-jikan <depaysement-jikan@gmail.com>
2026-03-18 20:34:16 -0500

    adding prometheus stack

--- modules/homelab/grafana/namespace/default.nix
+++ modules/homelab/grafana/namespace/default.nix
@@ -1,14 +1,20 @@
-{...}: {
-  services.k3s.manifests."grafana-namespace".content = [
-    {
-      apiVersion = "v1";
-      kind = "Namespace";
-      metadata = {
-        name = "grafana-system";
-        labels = {
-          "app.kubernetes.io/name" = "grafana";
+{
+  lib,
+  config,
+  ...
+}: {
+  config = lib.mkIf config.homelab.grafana.enable {
+    services.k3s.manifests."grafana-namespace".content = [
+      {
+        apiVersion = "v1";
+        kind = "Namespace";
+        metadata = {
+          name = "grafana-system";
+          labels = {
+            "app.kubernetes.io/name" = "grafana";
+          };
         };
-      };
-    }
-  ];
+      }
+    ];
+  };
 }
---
+++ modules/homelab/prometheus-stack/certificate/default.nix
@@ -0,0 +1,72 @@
+{
+  lib,
+  config,
+  ...
+}: {
+  config = lib.mkIf config.homelab.prometheus-stack.enable {
+    services.k3s = {
+      manifests."prometheus-cert".content = [
+        # --- Self-signed ClusterIssuer
+        {
+          apiVersion = "cert-manager.io/v1";
+          kind = "ClusterIssuer";
+          metadata = {
+            name = "prometheus-selfsigned";
+          };
+          spec = {
+            selfSigned = {};
+          };
+        }
+
+        # --- Certificate using self-signed issuer
+        {
+          apiVersion = "cert-manager.io/v1";
+          kind = "Certificate";
+          metadata = {
+            name = "prometheus-cert";
+            namespace = "monitoring";
+          };
+          spec = {
+            secretName = "prometheus-tls";
+            issuerRef = {
+              name = "prometheus-selfsigned";
+              kind = "ClusterIssuer";
+            };
+            dnsNames = ["prometheus.home"];
+          };
+        }
+      ];
+      manifests."grafana-cert".content = [
+        # --- Self-signed ClusterIssuer
+        {
+          apiVersion = "cert-manager.io/v1";
+          kind = "ClusterIssuer";
+          metadata = {
+            name = "grafana-selfsigned";
+          };
+          spec = {
+            selfSigned = {};
+          };
+        }
+
+        # --- Certificate using self-signed issuer
+        {
+          apiVersion = "cert-manager.io/v1";
+          kind = "Certificate";
+          metadata = {
+            name = "grafana-cert";
+            namespace = "monitoring";
+          };
+          spec = {
+            secretName = "grafana-tls";
+            issuerRef = {
+              name = "grafana-selfsigned";
+              kind = "ClusterIssuer";
+            };
+            dnsNames = ["grafana.home"];
+          };
+        }
+      ];
+    };
+  };
+}
