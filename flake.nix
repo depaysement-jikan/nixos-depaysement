eab0f13dbe1fb5532d9bbc4d839b482cf28def6f
Yoshua Carrera Vargas <yoshua.carrera24@gmail.com>
2026-03-21 17:44:45 -0500

    clean up comments

--- flake.nix
+++ flake.nix
@@ -2,14 +2,8 @@
   description = "Depaysement's Flake";
 
   inputs = {
-    # Nixpkgs
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
-    # You can access packages and modules from different nixpkgs revs
-    # at the same time. Here's an working example:
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-    # Also see the 'unstable-packages' overlay at 'overlays/default.nix'.
-
-    # Home manager
     home-manager = {
       url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -44,7 +38,6 @@
     ...
   } @ inputs: let
     inherit (self) outputs;
-    # Supported systems for your flake packages, shell, etc.
     systems = [
       "aarch64-linux"
       "i686-linux"
@@ -78,10 +71,6 @@
           disko.nixosModules.disko
           {networking = {inherit hostName;};}
         ];
-        # ++ nixpkgs.lib.optionals (builtins.pathExists ./hosts/${hostName}/disko) [
-        #   disko.nixosModules.disko
-        #   ./hosts/${hostName}/disko
-        # ];
       };
 
     mkHome = hostName: username: let
@@ -104,43 +93,23 @@
     inherit (hostEval) config;
     inherit (nixpkgs) lib;
 
-    # This is a function that generates an attribute by calling a function you
-    # pass to it, with each system as an argument
     forAllSystems = nixpkgs.lib.genAttrs systems;
   in {
-    # # Home Manager CLI per system
-    # home-manager =
-    #   forAllSystems (system: home-manager.packages.${system}.home-manager);
-
-    # Your custom packages
-    # Accessible through 'nix build', 'nix shell', etc
     packages =
       forAllSystems (system: import ./pkgs nixpkgs.legacyPackages.${system});
-
-    # Formatter for your nix files, available through 'nix fmt'
-    # Other options beside 'alejandra' include 'nixpkgs-fmt'
     formatter =
       forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
-    # Your custom packages and modifications, exported as overlays
     overlays = import ./overlays {inherit inputs;};
-    # Reusable nixos modules you might want to export
-    # These are usually stuff you would upstream into nixpkgs
     nixosModules = {
       default = import ./modules/nixos;
     };
-    # Reusable home-manager modules you might want to export
-    # These are usually stuff you would upstream into home-manager
     homeManagerModules = import ./modules/home-manager;
 
-    # NixOS configuration entrypoint
-    # Available through 'nixos-rebuild --flake .#your-hostname'
     nixosConfigurations =
       config.hosts
       |> lib.filterAttrs (_: attrs: attrs.platform == "nixos")
       |> lib.mapAttrs (hostName: attrs: mkHost hostName attrs);
 
-    # Standalone home-manager configuration entrypoint
-    # Available through 'home-manager --flake .#your-username@your-hostname'
     homeConfigurations =
       config.hosts
       |> lib.filterAttrs (_: attrs: attrs.platform != "mobile" && attrs.users != null)
