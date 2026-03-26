364cf414801b5ca980b2d772b603fb4391125314
depaysement-jikan <depaysement-jikan@gmail.com>
2026-03-25 21:57:14 -0500

    finish mkhost script

--- modules/homelab/security/sops.nix
+++ modules/homelab/security/sops.nix
@@ -13,9 +13,8 @@
     age = {
       sshKeyPaths = ["/var/lib/sops-nix/.ssh/${meta.hostname}"];
       # Instructions:
-      # mkdir -p ~/.config/sops/age
-      # age-keygen -o ~/.config/sops/age/keys.txt
-      # mkdir ~/.nixos-dotfiles/home-manager/secrets.yaml
+      # mkdir -p /var/lib/sops-nix/age
+      # age-keygen -o /var/lib/sops-nix/age/keys.txt
       # Fill in your secrets in YAML format
       # sudo sops --encrypt  --in-place --age $(sudo age-keygen -y /var/lib/sops-nix/age/key.txt) ~/.nixos-dotfiles/modules/homelab/secrets.yaml
       # home-manager switch --flake .
