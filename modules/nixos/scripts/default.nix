bf79e03e90a7d278a2d22dc70888ff8326cdcb2e
Yoshua Carrera Vargas <yoshua.carrera24@gmail.com>
2026-03-28 18:37:28 -0500

    logic abstraction

--- modules/nixos/scripts/default.nix
+++ modules/nixos/scripts/default.nix
@@ -7,6 +7,9 @@
   config = lib.mkIf config.nixos-generic.desktop.sddm.enable {
     environment.systemPackages = [
       (pkgs.writeShellScriptBin "mkHost" (builtins.readFile ./mkHost.sh))
+      (pkgs.writeShellScriptBin "mkUser" (builtins.readFile ./mkUser.sh))
+      (pkgs.writeShellScriptBin "shared/generateUserConfigFunctions.sh" (builtins.readFile ./shared/generateUserConfigFunctions.sh))
+      (pkgs.writeShellScriptBin "shared/generateHostConfigFunctions.sh" (builtins.readFile ./shared/generateHostConfigFunctions.sh))
     ];
   };
 }
