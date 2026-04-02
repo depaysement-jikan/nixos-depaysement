aeedc5b8b83a56d5bf0ad0587cda918b6dc2db49
Yoshua Carrera Vargas <yoshua.carrera24@gmail.com>
2026-04-02 14:06:05 -0500

    add some gum style

--- modules/nixos/scripts/mkHost.sh
+++ modules/nixos/scripts/mkHost.sh
@@ -9,6 +9,11 @@ source "$SCRIPT_DIR/shared/generateUserConfigFunctions.sh"
 # shellcheck source=shared/generateHostConfigFunctions.sh
 source "$SCRIPT_DIR/shared/generateHostConfigFunctions.sh"
 
+nix --extra-experimental-features "nix-command flakes pipe-operators" run nixpkgs#gum -- style \
+  --foreground 400 --border-foreground 400 --border double \
+  --align center --margin "1 1" --padding "1 1" \
+  'Host generator script'
+
 locateAndSetRepoDir
 
 getUserInputForHostCreation
--- modules/nixos/scripts/mkUser.sh
+++ modules/nixos/scripts/mkUser.sh
@@ -11,6 +11,11 @@ source "$SCRIPT_DIR/shared/generateUserConfigFunctions.sh"
 # shellcheck source=shared/generateHostConfigFunctions.sh
 source "$SCRIPT_DIR/shared/generateHostConfigFunctions.sh"
 
+nix --extra-experimental-features "nix-command flakes pipe-operators" run nixpkgs#gum -- style \
+  --foreground 400 --border-foreground 400 --border double \
+  --align center --margin "1 1" --padding "1 1" \
+  'User generator script'
+
 locateAndSetRepoDir
 
 getParentHostInputs
