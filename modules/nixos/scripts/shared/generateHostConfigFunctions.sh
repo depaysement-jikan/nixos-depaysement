b0060ce0ee50cc4b541028263ab9581de5626f48
Yoshua Carrera Vargas <yoshua.carrera24@gmail.com>
2026-04-03 18:49:29 -0500

    fix shell script tabbing

--- modules/nixos/scripts/shared/generateHostConfigFunctions.sh
+++ modules/nixos/scripts/shared/generateHostConfigFunctions.sh
@@ -56,78 +56,78 @@ EOF
 
 createHostDefaultConfig() {
   cat <<EOF >"$BASE_CONFIG_PATH/default.nix"
-  {
-    inputs,
-    outputs,
-    lib,
-    config,
-    pkgs,
-    ...
-  }: {
-    imports = [
-      ./hardware-configuration.nix
-      ./users
+{
+  inputs,
+  outputs,
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+  imports = [
+    ./hardware-configuration.nix
+    ./users
+  ];
+
+  nixpkgs = {
+    overlays = [
+      outputs.overlays.additions
+      outputs.overlays.modifications
+      outputs.overlays.unstable-packages
     ];
-
-    nixpkgs = {
-      overlays = [
-        outputs.overlays.additions
-        outputs.overlays.modifications
-        outputs.overlays.unstable-packages
-      ];
-      config = {
-        allowUnfree = true;
-      };
+    config = {
+      allowUnfree = true;
     };
+  };
 
-    nix = let
-      flakeInputs = lib.filterAttrs (_: lib.isType "flake") inputs;
-    in {
-      settings = {
-        experimental-features = ["nix-command" "flakes" "pipe-operators"];
-        flake-registry = "";
-        nix-path = config.nix.nixPath;
-      };
-      channel.enable = false;
-
-      registry = lib.mapAttrs (_: flake: {inherit flake;}) flakeInputs;
-      nixPath = lib.mapAttrsToList (n: _: "\${n}=flake:\${n}") flakeInputs;
+  nix = let
+    flakeInputs = lib.filterAttrs (_: lib.isType "flake") inputs;
+  in {
+    settings = {
+      experimental-features = ["nix-command" "flakes" "pipe-operators"];
+      flake-registry = "";
+      nix-path = config.nix.nixPath;
     };
+    channel.enable = false;
 
-    boot.loader.systemd-boot.enable = true;
-    boot.loader.efi.canTouchEfiVariables = true;
+    registry = lib.mapAttrs (_: flake: {inherit flake;}) flakeInputs;
+    nixPath = lib.mapAttrsToList (n: _: "\${n}=flake:\${n}") flakeInputs;
+  };
 
-    networking.hostName = "tsukinara";
-    networking.networkmanager.enable = true;
-    networking.firewall = {
-      enable = true;
-      allowedTCPPorts = [4200 3000];
-      trustedInterfaces = ["cni0" "flannel.1"];
-    };
-    networking.networkmanager.dns = "none";
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
 
-    networking.nameservers = [
-      "192.168.1.204"
-      "1.1.1.1"
-    ];
+  networking.hostName = "tsukinara";
+  networking.networkmanager.enable = true;
+  networking.firewall = {
+    enable = true;
+    allowedTCPPorts = [4200 3000];
+    trustedInterfaces = ["cni0" "flannel.1"];
+  };
+  networking.networkmanager.dns = "none";
 
-    time.timeZone = "${TIMEZONE}";
+  networking.nameservers = [
+    "192.168.1.204"
+    "1.1.1.1"
+  ];
 
-    environment.shells = with pkgs; [zsh git];
+  time.timeZone = "${TIMEZONE}";
 
-    environment.systemPackages = with pkgs; [bind];
-    programs.zsh.enable = true;
+  environment.shells = with pkgs; [zsh git];
 
-    services.openssh = {enable = true;};
-    services.blueman.enable = true;
+  environment.systemPackages = with pkgs; [bind];
+  programs.zsh.enable = true;
 
-    hardware.graphics = {
-      enable = true;
-      enable32Bit = true;
-    };
+  services.openssh = {enable = true;};
+  services.blueman.enable = true;
 
-    system.stateVersion = "25.11";
-  }
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
+
+  system.stateVersion = "25.11";
+}
 EOF
 
   CREATED_FILES+=("$BASE_CONFIG_PATH/default.nix")
@@ -137,99 +137,110 @@ createHostHomelabConfig() {
   mkdir -p "$BASE_CONFIG_PATH/config/homelab-config"
 
   cat <<EOF >"$BASE_CONFIG_PATH/config/homelab-config/default.nix"
-  {
-    lib,
-    config,
-    ...
-  }: {
-    options.homelab.enable = lib.mkEnableOption "Enable homelab module";
-    config = {
-      homelab = {
+{
+  lib,
+  config,
+  ...
+}: {
+  options.homelab.enable = lib.mkEnableOption "Enable homelab module";
+  config = {
+    homelab = {
+      enable = false;
+      flux = {
+        enable = true;
+        bucketName = "panaino";
+        endpoint = config.sops.placeholder.fluxEndpoint;
+        accessKeyId = config.sops.placeholder.fluxAccessKeyId;
+        secretAccessKey = config.sops.placeholder.fluxSecretKey;
+        webhook = config.sops.placeholder.fluxDiscordWebhookUrl;
+      };
+      ingress = {
+        enable = true;
+        resources = {
+          requests = {
+            cpu = "100m";
+            memory = "200Mi";
+          };
+          limits.memory = "400Mi";
+        };
+      };
+      vaultwarden = {
+        enable = true;
+        replicas = 1;
+        ingressHost = "vault.home";
+        loadBalancerIP = "192.168.1.201";
+        db = {
+          resources = {
+            requests = {
+              memory = "130Mi";
+            };
+            limits.memory = "200Mi";
+          };
+        };
+        resources = {
+          requests = {
+            memory = "50Mi";
+          };
+          limits.memory = "100Mi";
+        };
+      };
+      databases = {
+        enable = true;
+        cloudnative-pg = {
+          enable = true;
+        };
+      };
+      metallb = {
+        enable = true;
+        replicas = 1;
+        addresses = [
+          "192.168.1.201-192.168.1.254"
+        ];
+      };
+      longhorn = {
+        # TODO: This is causing issues with flannel generation, disabled for now, Flux might fix it
+        # context: https://github.com/k3s-io/k3s/issues/13277#issuecomment-3837472085
         enable = false;
-        flux = {
-          enable = true;
-          bucketName = "panaino";
-          endpoint = config.sops.placeholder.fluxEndpoint;
-          accessKeyId = config.sops.placeholder.fluxAccessKeyId;
-          secretAccessKey = config.sops.placeholder.fluxSecretKey;
-          webhook = config.sops.placeholder.fluxDiscordWebhookUrl;
+        replicas = 1;
+        ingressHost = "longhorn.home";
+      };
+      immich = {
+        enable = true;
+        replicas = 1;
+        ingressHost = "immich.home";
+        storageClass = "local-path";
+        db = {
+          instances = 1;
+          size = "1Gi";
         };
-        ingress = {
-          enable = true;
-          resources = {
-            requests = {
-              cpu = "100m";
-              memory = "200Mi";
-            };
-            limits.memory = "400Mi";
-          };
-        };
-        vaultwarden = {
-          enable = true;
-          replicas = 1;
-          ingressHost = "vault.home";
-          loadBalancerIP = "192.168.1.201";
-          db = {
-            resources = {
-              requests = {
-                memory = "130Mi";
-              };
-              limits.memory = "200Mi";
-            };
-          };
-          resources = {
-            requests = {
-              memory = "50Mi";
-            };
-            limits.memory = "100Mi";
-          };
-        };
-        databases = {
-          enable = true;
-          cloudnative-pg = {
-            enable = true;
-          };
-        };
-        metallb = {
-          enable = true;
-          replicas = 1;
-          addresses = [
-            "192.168.1.201-192.168.1.254"
-          ];
-        };
-        longhorn = {
-          # TODO: This is causing issues with flannel generation, disabled for now, Flux might fix it
-          # context: https://github.com/k3s-io/k3s/issues/13277#issuecomment-3837472085
-          enable = false;
-          replicas = 1;
-          ingressHost = "longhorn.home";
-        };
-        immich = {
-          enable = true;
-          replicas = 1;
-          ingressHost = "immich.home";
-          storageClass = "local-path";
-          db = {
-            instances = 1;
-            size = "1Gi";
-          };
-        };
-        pihole = {
-          enable = true;
-          password = config.sops.placeholder.piholePassword;
-          gated = false;
-          webLoadBalancerIP = "192.168.1.204";
-          dnsLoadBalancerIP = "192.168.1.204";
-          dns = "192.168.1.1";
-        };
-        cert-manager = {
-          enable = true;
-          email = config.sops.placeholder.certEmail;
-        };
-        tailscale = {
-          enable = true;
-          authKeyFile = config.sops.secrets.tailscaleAuthKey.path;
-        };
+      };
+      pihole = {
+        enable = true;
+        password = config.sops.placeholder.piholePassword;
+        gated = false;
+        webLoadBalancerIP = "192.168.1.204";
+        dnsLoadBalancerIP = "192.168.1.204";
+        dns = "192.168.1.1";
+      };
+      cert-manager = {
+        enable = true;
+        email = config.sops.placeholder.certEmail;
+      };
+      tailscale = {
+        enable = true;
+        authKeyFile = config.sops.secrets.tailscaleAuthKey.path;
+      };
+      prometheus = {
+        enable = false;
+        ingressHost = "prometheus.home";
+      };
+      grafana = {
+        enable = false;
+        ingressHost = "grafana.home";
+        loadBalancerIP = "192.168.1.210";
+      };
+      prometheus-stack = {
+        enable = true;
         prometheus = {
           enable = false;
           ingressHost = "prometheus.home";
@@ -239,39 +250,28 @@ createHostHomelabConfig() {
           ingressHost = "grafana.home";
           loadBalancerIP = "192.168.1.210";
         };
-        prometheus-stack = {
-          enable = true;
-          prometheus = {
-            enable = false;
-            ingressHost = "prometheus.home";
-          };
-          grafana = {
-            enable = false;
-            ingressHost = "grafana.home";
-            loadBalancerIP = "192.168.1.210";
-          };
-        };
-        uptime-kuma = {
-          enable = true;
-          ingressHost = "kuma.home";
-          loadBalancerIP = "192.168.1.209";
-        };
-        forgejo = {
-          enable = true;
-          ingressHost = "forgejo.home";
-          httpLoadBalancerIP = "192.168.1.212";
-          sshLoadBalancerIP = "192.168.1.213";
-        };
+      };
+      uptime-kuma = {
+        enable = true;
+        ingressHost = "kuma.home";
+        loadBalancerIP = "192.168.1.209";
+      };
+      forgejo = {
+        enable = true;
+        ingressHost = "forgejo.home";
+        httpLoadBalancerIP = "192.168.1.212";
+        sshLoadBalancerIP = "192.168.1.213";
+      };
 
-        # TODO: Future configs
+      # TODO: Future configs
 
-        garage = {
-          enable = true;
-          ingressHost = null;
-        };
+      garage = {
+        enable = true;
+        ingressHost = null;
       };
     };
-  }
+  };
+}
 EOF
 
   CREATED_FILES+=("$BASE_CONFIG_PATH/config/homelab-config/default.nix")
@@ -281,18 +281,18 @@ createHostNixosConfig() {
   mkdir -p "$BASE_CONFIG_PATH/config/nixos-config"
 
   cat <<EOF >"$BASE_CONFIG_PATH/config/nixos-config/default.nix"
-  {...}: {
-    config = {
-      nixos-generic = {
-        desktop = {
-          enable = true;
-          sddm.enable = true;
-          hyprland.enable = true;
-          homeManager.enable = true;
-        };
+{...}: {
+  config = {
+    nixos-generic = {
+      desktop = {
+        enable = true;
+        sddm.enable = true;
+        hyprland.enable = true;
+        homeManager.enable = true;
       };
     };
-  }
+  };
+}
 EOF
 
   CREATED_FILES+=("$BASE_CONFIG_PATH/config/nixos-config/default.nix")
