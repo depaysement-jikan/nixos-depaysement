86787624406a6ee4e1ac7f2f1632f4f1773eff34
Yoshua Carrera Vargas <yoshua.carrera24@gmail.com>
2026-04-03 18:51:43 -0500

    adding a new host and user

---
+++ hosts/shinobu/users/kokoro/default.nix
@@ -0,0 +1,17 @@
+{
+  pkgs,
+  config,
+  ...
+}: {
+  imports = [
+    ./security/sops.nix
+  ];
+  users.users.kokoro = {
+    isNormalUser = true;
+    extraGroups = ["wheel" "k3s" "sddm"];
+    packages = with pkgs; [tree kitty];
+    shell = pkgs.zsh;
+    hashedPasswordFile = config.sops.secrets.userHashedPassword.path;
+    homeMode = "711";
+  };
+}
---
+++ hosts/shinobu/users/kokoro/security/sops.nix
@@ -0,0 +1,44 @@
+{
+  config,
+  inputs,
+  pkgs,
+  ...
+}: {
+  imports = [inputs.sops-nix.nixosModules.sops];
+
+  environment.systemPackages = builtins.attrValues {inherit (pkgs) age sops;};
+
+  sops = {
+    age = {
+      sshKeyPaths = ["/var/lib/sops-nix/.ssh/kokoro"];
+      # Instructions:
+      # mkdir -p /var/lib/sops-nix/age
+      # age-keygen -o /var/lib/sops-nix/age/keys.txt
+      # Fill in your secrets in YAML format
+      # sudo sops --encrypt  --in-place --age $(sudo age-keygen -y /var/lib/sops-nix/age/key.txt) "/home/depaysement/.nixos-dotfiles/hosts/shinobu"/hosts/shinobu/users/kokoro/secrets.yaml
+      # sudo nixos-rebuild switch --flake .#tsukinara
+      keyFile = "/var/lib/sops-nix/age/key.txt";
+    };
+    secrets = {
+      userHashedPassword = {
+        sopsFile = ../secrets.yaml;
+      };
+      userGitName = {
+        sopsFile = ../secrets.yaml;
+      };
+      userGitEmail = {
+        sopsFile = ../secrets.yaml;
+      };
+    };
+    templates.git-user = {
+      path = "/home/kokoro/.config/git/user.gitconfig";
+      mode = "0644";
+      owner = "kokoro";
+      content = ''
+        [user]
+          name = ${config.sops.placeholder.userGitName}
+          email = ${config.sops.placeholder.userGitEmail}
+      '';
+    };
+  };
+}
