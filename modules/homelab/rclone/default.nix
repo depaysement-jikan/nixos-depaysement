aa28538eadccf3610cd0b83e935c8cb5258962be
Yoshua Carrera Vargas <yoshua.carrera24@gmail.com>
2026-04-05 17:44:41 -0500

    tie prometheus prometheus stack rclone

--- modules/homelab/rclone/default.nix
+++ modules/homelab/rclone/default.nix
@@ -1,42 +1,45 @@
 {
   config,
   pkgs,
+  lib,
   ...
 }: {
-  environment.systemPackages = with pkgs; [rclone];
-  sops.templates.rclone = {
-    content = ''
-      [Namishiro]
-      type = s3
-      provider = Cloudflare
-      access_key_id = ${config.sops.placeholder.fluxAccessKeyId}
-      secret_access_key = ${config.sops.placeholder.fluxSecretKey}
-      region = auto
-      endpoint = https://${config.sops.placeholder.fluxEndpoint}
-    '';
-    path = "/etc/rclone.conf";
-  };
-
-  systemd.services.flux-s3-sync = {
-    description = "Sync Nix-generated manifests to S3 for Flux";
-
-    wantedBy = ["multi-user.target"];
-
-    wants = ["network-online.target"];
-    after = ["network-online.target"];
-
-    serviceConfig = {
-      Type = "oneshot";
-      User = "root";
+  config = lib.mkIf (config.homelab.flux.enable && config.homelab.enable) {
+    environment.systemPackages = with pkgs; [rclone];
+    sops.templates.rclone = {
+      content = ''
+        [Namishiro]
+        type = s3
+        provider = Cloudflare
+        access_key_id = ${config.sops.placeholder.fluxAccessKeyId}
+        secret_access_key = ${config.sops.placeholder.fluxSecretKey}
+        region = auto
+        endpoint = https://${config.sops.placeholder.fluxEndpoint}
+      '';
+      path = "/etc/rclone.conf";
     };
 
-    script = ''
-      ${pkgs.rclone}/bin/rclone sync ${config.services.k3s.manifestDir} Namishiro:${config.homelab.flux.bucketName} \
-        --config /etc/rclone.conf \
-        --checksum \
-        --verbose \
-        --copy-links \
-        --s3-no-check-bucket
-    '';
+    systemd.services.flux-s3-sync = {
+      description = "Sync Nix-generated manifests to S3 for Flux";
+
+      wantedBy = ["multi-user.target"];
+
+      wants = ["network-online.target"];
+      after = ["network-online.target"];
+
+      serviceConfig = {
+        Type = "oneshot";
+        User = "root";
+      };
+
+      script = ''
+        ${pkgs.rclone}/bin/rclone sync ${config.services.k3s.manifestDir} Namishiro:${config.homelab.flux.bucketName} \
+          --config /etc/rclone.conf \
+          --checksum \
+          --verbose \
+          --copy-links \
+          --s3-no-check-bucket
+      '';
+    };
   };
 }
