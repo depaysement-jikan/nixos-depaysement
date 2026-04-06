1cfe5fd023822b4cd9c4982b31fc5309eeb921d3
Yoshua Carrera Vargas <yoshua.carrera24@gmail.com>
2026-04-05 20:13:55 -0500

    minor fixes

--- hosts/shinobu/users/kokoro/security/sops.nix
+++ hosts/shinobu/users/kokoro/security/sops.nix
@@ -15,7 +15,7 @@
       # mkdir -p /var/lib/sops-nix/age
       # age-keygen -o /var/lib/sops-nix/age/keys.txt
       # Fill in your secrets in YAML format
-      # sudo sops --encrypt  --in-place --age $(sudo age-keygen -y /var/lib/sops-nix/age/key.txt) "/home/depaysement/.nixos-dotfiles/hosts/shinobu"/hosts/shinobu/users/kokoro/secrets.yaml
+      # sudo sops --encrypt  --in-place --age $(sudo age-keygen -y /var/lib/sops-nix/age/key.txt) ~/.nixos-dotfiles/hosts/shinobu/users/kokoro/secrets.yaml
       # sudo nixos-rebuild switch --flake .#tsukinara
       keyFile = "/var/lib/sops-nix/age/key.txt";
     };
