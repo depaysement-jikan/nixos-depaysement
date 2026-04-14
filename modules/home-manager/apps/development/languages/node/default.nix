ebc5aa83e041a6c9ab0b47525539eb588affc633
yoshua carrera vargas <yoshua.carrera24@gmail.com>
2026-04-13 23:30:30 -0500

    add gemini cli to home manager

--- modules/home-manager/apps/development/languages/node/default.nix
+++ modules/home-manager/apps/development/languages/node/default.nix
@@ -6,6 +6,6 @@
 }: {
   options = {node.enable = lib.mkEnableOption "Enable node module";};
   config = lib.mkIf config.homeManager.apps.development.languages.node.enable {
-    home.packages = with pkgs; [nodejs_20 pnpm yarn deno prettier];
+    home.packages = with pkgs; [nodejs_20 pnpm yarn deno prettier gemini-cli];
   };
 }
