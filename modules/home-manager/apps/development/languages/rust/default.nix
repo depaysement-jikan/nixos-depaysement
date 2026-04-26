3727ea772451cdfcd4ef69d346105ec197d15730
yoshua carrera vargas <yoshua.carrera24@gmail.com>
2026-04-25 21:09:05 -0500

    rust module refactor

--- modules/home-manager/apps/development/languages/rust/default.nix
+++ modules/home-manager/apps/development/languages/rust/default.nix
@@ -6,6 +6,28 @@
 }: {
   options = {rust.enable = lib.mkEnableOption "Enable rust module";};
   config = lib.mkIf config.homeManager.apps.development.languages.rust.enable {
-    home.packages = with pkgs; [dioxus-cli rustc rustup];
+    home.packages = with pkgs; [
+      rustup
+      openssl.dev
+      pkg-config
+      (pkgs.rustPlatform.buildRustPackage {
+        pname = "dioxus-cli";
+        version = "0.7.6";
+        src = pkgs.fetchCrate {
+          pname = "dioxus-cli";
+          version = "0.7.6";
+          sha256 = "sha256-PKidohK85wv/ZN9WcNS+HTlVGgR5o07gWLshZhzyg5k=";
+        };
+        nativeBuildInputs = [
+          pkgs.pkg-config
+        ];
+        buildInputs = [
+          pkgs.openssl.dev
+        ];
+        OPENSSL_NO_VENDOR = 1;
+        doCheck = false;
+        cargoHash = "sha256-T6xLlu8XeJPm+ULgpTALTT93X55ExJhDMuhpal2QLhg=";
+      })
+    ];
   };
 }
