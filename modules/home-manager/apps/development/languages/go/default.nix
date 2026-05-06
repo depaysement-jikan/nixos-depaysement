085f8a30d71ef7adbc95590fbc290272c0833406
depaysement-jikan <depaysement-jikan@gmail.com>
2026-05-05 22:51:53 -0500

    add go air

--- modules/home-manager/apps/development/languages/go/default.nix
+++ modules/home-manager/apps/development/languages/go/default.nix
@@ -6,6 +6,6 @@
 }: {
   options = {go.enable = lib.mkEnableOption "Enable go module";};
   config = lib.mkIf config.homeManager.apps.development.languages.go.enable {
-    home.packages = with pkgs; [go gopls];
+    home.packages = with pkgs; [go gopls gofumpt air];
   };
 }
