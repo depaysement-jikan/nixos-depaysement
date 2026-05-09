c79b008b45c8082ff60ec2a3af6e147c2e05eb2b
yoshua carrera vargas <yoshua.carrera24@gmail.com>
2026-05-08 21:12:50 -0500

    add gum to home manager

--- modules/home-manager/apps/development/languages/go/default.nix
+++ modules/home-manager/apps/development/languages/go/default.nix
@@ -6,6 +6,6 @@
 }: {
   options = {go.enable = lib.mkEnableOption "Enable go module";};
   config = lib.mkIf config.homeManager.apps.development.languages.go.enable {
-    home.packages = with pkgs; [go gopls gofumpt air];
+    home.packages = with pkgs; [go gopls gofumpt air gum];
   };
 }
