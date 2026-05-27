81f537e6feb52af8c8cd491a801951d932df5b0a
yoshua carrera vargas <yoshua.carrera24@gmail.com>
2026-05-26 23:28:16 -0500

    adding elixir phoenix

--- modules/home-manager/apps/development/languages/elixir/default.nix
+++ modules/home-manager/apps/development/languages/elixir/default.nix
@@ -6,6 +6,18 @@
 }: {
   options = {elixir.enable = lib.mkEnableOption "Enable elixir module";};
   config = lib.mkIf config.homeManager.apps.development.languages.elixir.enable {
-    home.packages = with pkgs; [elixir erlang elixir-ls];
+    home.packages = with pkgs; [
+      elixir
+      erlang
+      elixir-ls
+      beamPackages.hex
+      beamPackages.rebar3
+      inotify-tools
+    ];
+    home.activation.installPhoenix = lib.hm.dag.entryAfter ["writeBoundary"] ''
+      if ! ${pkgs.elixir}/bin/mix archive | grep -q phx_new; then
+        ${pkgs.elixir}/bin/mix archive.install hex phx_new --force
+      fi
+    '';
   };
 }
