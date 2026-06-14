f7022c95e12873d8ab3b7d3f1fee1878774d0866
yoshua carrera vargas <yoshua.carrera24@gmail.com>
2026-06-13 22:38:14 -0500

    fix typo on zsh rc

--- modules/home-manager/apps/development/terminal/zsh/default.nix
+++ modules/home-manager/apps/development/terminal/zsh/default.nix
@@ -82,7 +82,7 @@
           alias cd='z'
           alias git-co='sh ~/.config/scripts/fuzzy-co.sh'
           alias k='kubectl'
-          alis g='git'
+          alias g='git'
 
           # Nuke starship if needed
           # sed -i '/starship/d' ~/.zshrc
