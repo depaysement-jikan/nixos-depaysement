ce44faa507254cf8a0d1b094f6bc4edb73c99ada
yoshua carrera vargas <yoshua.carrera24@gmail.com>
2026-06-24 22:20:59 -0500

    update shinobu config

--- hosts/shinobu/users/kokoro/config/home-manager-config/default.nix
+++ hosts/shinobu/users/kokoro/config/home-manager-config/default.nix
@@ -82,6 +82,7 @@
         hyprland.enable = true;
         hyprlock.enable = true;
         waybar.enable = true;
+        noctalia.enable = false;
       };
       system = {
         enable = true;
