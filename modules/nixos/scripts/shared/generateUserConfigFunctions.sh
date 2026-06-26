6db61ea34bd435d73f1d2303b79b6ffe7ec88135
depaysement-jikan <depaysement-jikan@gmail.com>
2026-06-25 23:27:02 -0500

    fix user generation script

--- modules/nixos/scripts/shared/generateUserConfigFunctions.sh
+++ modules/nixos/scripts/shared/generateUserConfigFunctions.sh
@@ -52,7 +52,7 @@ createUserDefaultConfig() {
     extraGroups = ["wheel" "k3s" "sddm"];
     packages = with pkgs; [tree kitty];
     shell = pkgs.zsh;
-    password = "12345"
+    password = "12345";
     homeMode = "711";
   };
   users.mutableUsers = false;
