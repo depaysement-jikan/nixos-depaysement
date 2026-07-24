2b2cc43704b4ee86a3577c75a569c24e8d419ae3
depaysement-jikan <depaysement-jikan@gmail.com>
2026-07-23 21:47:46 -0500

    add cmp tw

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -2,8 +2,16 @@ return {
   "hrsh7th/nvim-cmp",
   dependencies = {
     "hrsh7th/cmp-nvim-lsp",
-    "L3MON4D3/LuaSnip", -- required even if you don't use snippets
+    "L3MON4D3/LuaSnip",
+    "roobert/tailwindcss-colorizer-cmp.nvim",
   },
+  opts = function(_, opts)
+    local format_kinds = opts.formatting.format
+    opts.formatting.format = function(entry, item)
+      format_kinds(entry, item)
+      return require("tailwindcss-colorizer-cmp").formatter(entry, item)
+    end
+  end,
   config = function()
     local cmp = require("cmp")
 
