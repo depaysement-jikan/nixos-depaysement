665c537ab4ad4443ce0c3e9494daec39d576a39d
depaysement-jikan <depaysement-jikan@gmail.com>
2026-07-27 22:50:03 -0500

    add sodachi host and user nix config

---
+++ hosts/sodachi/default.nix
@@ -0,0 +1,72 @@
+{
+  inputs,
+  outputs,
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+  imports = [
+    ./hardware-configuration.nix
+    ./users
+  ];
+
+  nixpkgs = {
+    overlays = [
+      outputs.overlays.additions
+      outputs.overlays.modifications
+      outputs.overlays.unstable-packages
+    ];
+    config = {
+      allowUnfree = true;
+    };
+  };
+
+  nix = let
+    flakeInputs = lib.filterAttrs (_: lib.isType "flake") inputs;
+  in {
+    settings = {
+      experimental-features = ["nix-command" "flakes" "pipe-operators"];
+      flake-registry = "";
+      nix-path = config.nix.nixPath;
+    };
+    channel.enable = false;
+
+    registry = lib.mapAttrs (_: flake: {inherit flake;}) flakeInputs;
+    nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
+  };
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "sodachi";
+  networking.networkmanager.enable = true;
+  networking.firewall = {
+    enable = true;
+    allowedTCPPorts = [4200 3000];
+    trustedInterfaces = ["cni0" "flannel.1"];
+  };
+  networking.networkmanager.dns = "none";
+
+  networking.nameservers = [
+    "192.168.1.204"
+    "1.1.1.1"
+  ];
+
+  time.timeZone = "America/Chicago";
+
+  environment.shells = with pkgs; [zsh git];
+
+  environment.systemPackages = with pkgs; [bind];
+  programs.zsh.enable = true;
+
+  services.openssh = {enable = true;};
+  services.blueman.enable = true;
+
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
+
+  system.stateVersion = "25.11";
+}
