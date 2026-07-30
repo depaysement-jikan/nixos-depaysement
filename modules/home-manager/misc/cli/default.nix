859b36dca8c8ac007b5a9daa386cb89493dd2b31
depaysement-jikan <depaysement-jikan@gmail.com>
2026-07-29 23:06:57 -0500

    add htop

--- modules/home-manager/misc/cli/default.nix
+++ modules/home-manager/misc/cli/default.nix
@@ -6,6 +6,6 @@
 }: {
   options = {cli.enable = lib.mkEnableOption "Enable misc cli module";};
   config = lib.mkIf config.homeManager.misc.cli.enable {
-    home.packages = with pkgs; [cbonsai lolcat fastfetch];
+    home.packages = with pkgs; [cbonsai lolcat fastfetch htop];
   };
 }
