917cf22ad41466bdc73b65e96b613055b661faa5
depaysement-jikan <depaysement-jikan@gmail.com>
2026-07-30 23:49:50 -0500

    change to nu on tsukinara

--- hosts/tsukinara/users/depaysement/default.nix
+++ hosts/tsukinara/users/depaysement/default.nix
@@ -6,7 +6,7 @@
     isNormalUser = true;
     extraGroups = ["wheel" "k3s" "sddm"];
     packages = with pkgs; [tree kitty];
-    shell = pkgs.zsh;
+    shell = pkgs.nu;
     hashedPassword = "$6$0.0l2IumZW8Hx98U$oI6ohUZ8/68s./AzWxd734C6oSbvNiAeYQgxIXH4FVkXOOdKfImRZxShH7DFAFj9ZAUFnSNf8KJP2XTzWNIRq1";
     homeMode = "711";
   };
