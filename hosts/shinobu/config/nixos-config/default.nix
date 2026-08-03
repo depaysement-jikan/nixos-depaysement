0b38f6bf1804451d0f24c0c5e3d4bee84c6a3f1b
yoshua carrera vargas <yoshua.carrera24@gmail.com>
2026-08-03 13:45:23 -0500

    enable tuigreet on shinobu

--- hosts/shinobu/config/nixos-config/default.nix
+++ hosts/shinobu/config/nixos-config/default.nix
@@ -3,7 +3,8 @@
     nixos-generic = {
       desktop = {
         enable = true;
-        sddm.enable = true;
+        sddm.enable = false;
+        tuigreet.enable = true;
         hyprland.enable = true;
         homeManager.enable = true;
         audio.enable = true;
