de3988b551b7e64f5d35b2701050f469c01cb478
yoshua carrera vargas <yoshua.carrera24@gmail.com>
2026-08-03 13:45:41 -0500

    add tuigreet as a disabled option on the rest of the hosts

--- hosts/tsukinara/config/nixos-config/default.nix
+++ hosts/tsukinara/config/nixos-config/default.nix
@@ -4,6 +4,7 @@
       desktop = {
         enable = true;
         sddm.enable = true;
+        tuigreet.enable = false;
         hyprland.enable = true;
         homeManager.enable = true;
         audio.enable = true;
