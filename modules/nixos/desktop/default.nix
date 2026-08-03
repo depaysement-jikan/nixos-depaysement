1a2ecb6562d9b615f534e3867848e369fc382309
depaysement-jikan <depaysement-jikan@gmail.com>
2026-08-03 13:45:12 -0500

    add option for tuigreet

--- modules/nixos/desktop/default.nix
+++ modules/nixos/desktop/default.nix
@@ -5,7 +5,7 @@
 }: let
   cfg = config.nixos-generic.desktop;
 in {
-  imports = [./sddm ./hyprland ./home-manager ./audio];
+  imports = [./sddm ./hyprland ./home-manager ./audio ./tuigreet];
   options.nixos-generic.desktop = {
     enable = lib.mkEnableOption "Desktop environment";
   };
@@ -13,6 +13,7 @@ in {
   config = lib.mkIf cfg.enable {
     nixos-generic.desktop = {
       sddm.enable = lib.mkDefault true;
+      tuigreet.enable = lib.mkDefault true;
       hyprland.enable = lib.mkDefault true;
       homeManager.enable = lib.mkDefault true;
       audio.enable = lib.mkDefault true;
