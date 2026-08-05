fce8277dfe9ebb63592ea320597c23315e435881
yoshua carrera vargas <yoshua.carrera24@gmail.com>
2026-08-05 13:52:58 -0500

    move open link hub out of host config

--- hosts/shinobu/default.nix
+++ hosts/shinobu/default.nix
@@ -71,31 +71,6 @@
   services.openssh = {enable = true;};
   services.blueman.enable = true;
   services.hardware.openrgb.enable = true;
-  systemd.services.openlinkhub = {
-    description = "OpenLinkHub";
-    wantedBy = ["multi-user.target"];
-
-    serviceConfig = {
-      WorkingDirectory = "/home/kokoro/code/OpenLinkHub";
-      ExecStart = "/home/kokoro/.nix-profile/bin/OpenLinkHub";
-
-      User = "root";
-      Group = "root";
-
-      Restart = "on-failure";
-      RestartSec = 5;
-    };
-  };
-  services.nginx = {
-    enable = true;
-
-    virtualHosts."rgb.localhost" = {
-      locations."/" = {
-        proxyPass = "http://127.0.0.1:27003";
-      };
-    };
-  };
-
   hardware.graphics = {
     enable = true;
     enable32Bit = true;
