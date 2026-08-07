dc6b9393a6392324d3505ee86fa286ddcc31c210
yoshua carrera vargas <yoshua.carrera24@gmail.com>
2026-08-07 12:27:24 -0500

    update treesitter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,4 +1,7 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   commit = "8f3e5a7",
+  opts = {
+    ensure_installed = { "go", "rust", "typescript", "javascript", "elixir", "lua", "zig", "html", "css", "javascript" },
+  },
 }
