40bef403abab3c6f96686024ad3480d54f1ded06
depaysement-jikan <depaysement-jikan@gmail.com>
2026-08-06 23:14:51 -0500

    improve zsh config

--- modules/home-manager/apps/development/terminal/zsh/default.nix
+++ modules/home-manager/apps/development/terminal/zsh/default.nix
@@ -4,50 +4,76 @@
   config,
   ...
 }: {
-  options = {zsh.enable = lib.mkEnableOption "Enable zsh module";};
+  options = {
+    zsh.enable = lib.mkEnableOption "Enable zsh module";
+  };
+
   config = lib.mkIf config.homeManager.apps.development.terminal.zsh.enable {
     home = {
-      shell = {enableZshIntegration = true;};
-      sessionPath = ["$HOME/.local/share/pnpm"];
-      sessionVariables = {PNPM_HOME = "$HOME/.local/share/pnpm";};
-      packages = with pkgs; [zoxide ripgrep fd fzf openssl bat];
+      shell = {
+        enableZshIntegration = true;
+      };
+
+      sessionPath = [
+        "$HOME/.local/bin"
+        "$HOME/.local/share/pnpm"
+        "/tmp/lazy-lvim/bin"
+      ];
+
+      sessionVariables = {
+        PNPM_HOME = "$HOME/.local/share/pnpm";
+      };
+
+      packages = with pkgs; [
+        zoxide
+        ripgrep
+        fd
+        fzf
+        openssl
+        bat
+      ];
     };
+
     programs = {
       eza.enable = true;
+
       zsh = {
         enable = true;
         enableCompletion = true;
+
         autosuggestion.enable = true;
         syntaxHighlighting.enable = true;
-        initContent = ''
-          export PATH=$PATH:~/.local/bin/
-          export PATH=/tmp/lazy-lvim/bin:$PATH
 
+        initContent = ''
           # Autosuggest
           ZSH_AUTOSUGGEST_USE_ASYNC="true"
 
-          # Group matches and describe.
+          # Completion
           zstyle ':completion:*' sort false
           zstyle ':completion:complete:*:options' sort false
-          zstyle ':completion:*' matcher-list 'm:{[:lower:][:upper:]}={[:upper:][:lower:]}' 'm:{[:lower:][:upper:]}={[:upper:][:lower:]} l:|=* r:|=*' 'm:{[:lower:][:upper:]}={[:upper:][:lower:]} l:|=* r:|=*' 'm:{[:lower:][:upper:]}={[:upper:][:lower:]} l:|=* r:|=*'
+
+          zstyle ':completion:*' matcher-list \
+            'm:{[:lower:][:upper:]}={[:upper:][:lower:]}' \
+            'm:{[:lower:][:upper:]}={[:upper:][:lower:]} l:|=* r:|=*'
+
           zstyle ':completion:*' special-dirs true
-          zstyle ':completion:*' rehash true
 
           zstyle ':completion:*' menu yes select
           zstyle ':completion:*' list-grouped false
-          zstyle ':completion:*' list-separator '''
-          zstyle ':completion:*' group-name '''
+          zstyle ':completion:*' list-separator ""
+          zstyle ':completion:*' group-name ""
           zstyle ':completion:*' verbose yes
-          zstyle ':completion:*:matches' group 'yes'
+          zstyle ':completion:*:matches' group yes
+
           zstyle ':completion:*:warnings' format '%F{red}%B-- No match for: %d --%b%f'
           zstyle ':completion:*:messages' format '%d'
           zstyle ':completion:*:corrections' format '%B%d (errors: %e)%b'
           zstyle ':completion:*:descriptions' format '[%d]'
 
-          # Fuzzy match mistyped completions.
-          zstyle ':completion:*' completer _complete _match _approximate
+          # Faster than including _approximate
+          zstyle ':completion:*' completer _complete _match
+
           zstyle ':completion:*:match:*' original only
-          zstyle ':completion:*:approximate:*' max-errors 1 numeric
 
           # Don't complete unavailable commands.
           zstyle ':completion:*:functions' ignored-patterns '(_*|pre(cmd|exec))'
@@ -56,24 +82,25 @@
           zstyle ':completion:*:*:-subscript-:*' tag-order indexes parameters
 
           # fzf-tab
-          source ${pkgs.zsh-fzf-tab}/share/fzf-tab/fzf-tab.plugin.zsh
           zstyle ':fzf-tab:complete:_zlua:*' query-string input
-          zstyle ':fzf-tab:complete:kill:argument-rest' fzf-preview 'ps --pid=$word -o cmd --no-headers -w -w'
-          zstyle ':fzf-tab:complete:kill:argument-rest' fzf-flags '--preview-window=down:3:wrap'
+          zstyle ':fzf-tab:complete:kill:argument-rest' \
+            fzf-preview 'ps --pid=$word -o cmd --no-headers -w -w'
+          zstyle ':fzf-tab:complete:kill:argument-rest' \
+            fzf-flags '--preview-window=down:3:wrap'
           zstyle ':fzf-tab:complete:kill:*' popup-pad 0 3
-          zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza -1 --color=always $realpath'
+          zstyle ':fzf-tab:complete:cd:*' \
+            fzf-preview 'eza -1 --color=always $realpath'
           zstyle ':fzf-tab:complete:cd:*' popup-pad 30 0
           zstyle ':fzf-tab:*' switch-group ',' '.'
+
           zstyle ":completion:*:git-checkout:*" sort false
           zstyle ':completion:*' file-sort modification
-          # zstyle ':completion:*:eza' sort false
           zstyle ':completion:files' sort false
 
           # zoxide
           eval "$(zoxide init zsh)"
-          eval "$(ssh-agent -s)" > /dev/null 2>&1
 
-          #starship
+          # starship
           eval "$(starship init zsh)"
 
           # Aliases
@@ -83,13 +110,16 @@
           alias git-co='sh ~/.config/scripts/fuzzy-co.sh'
           alias k='kubectl'
           alias g='git'
-
-          # Nuke starship if needed
-          # sed -i '/starship/d' ~/.zshrc
         '';
+
         oh-my-zsh = {
           enable = true;
-          plugins = ["git" "colorize" "colored-man-pages" "history-substring-search"];
+          plugins = [
+            "git"
+            "colorize"
+            "colored-man-pages"
+            "history-substring-search"
+          ];
         };
 
         plugins = with pkgs; [
@@ -98,6 +128,7 @@
             file = "zsh-autopair.plugin.zsh";
             src = zsh-autopair;
           }
+
           {
             name = "fzf-tab";
             file = "fzf-tab.plugin.zsh";
