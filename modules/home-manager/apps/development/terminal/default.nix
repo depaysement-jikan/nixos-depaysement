707479a601c1799841be18648ed26ec4de5b39ed
depaysement-jikan <depaysement-jikan@gmail.com>
2026-08-08 23:05:31 -0500

    import and add foot config

--- modules/home-manager/apps/development/terminal/default.nix
+++ modules/home-manager/apps/development/terminal/default.nix
@@ -3,7 +3,7 @@
   config,
   ...
 }: {
-  imports = [./yazi ./zsh ./nushell ./tmux ./git ./ghostty ./nvim ./starship ./certbot ./doppler];
+  imports = [./yazi ./zsh ./nushell ./tmux ./git ./ghostty ./nvim ./starship ./certbot ./doppler ./foot];
 
   options = {terminal.enable = lib.mkEnableOption "Enable terminal module";};
   config = lib.mkIf config.homeManager.apps.development.terminal.enable {
@@ -13,6 +13,7 @@
     tmux.enable = lib.mkDefault true;
     git.enable = lib.mkDefault true;
     ghostty.enable = lib.mkDefault true;
+    foot.enable = lib.mkDefault true;
     starship.enable = lib.mkDefault true;
     certbot.enable = lib.mkDefault true;
     doppler.enable = lib.mkDefault true;
