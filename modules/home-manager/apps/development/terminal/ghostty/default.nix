8d9588831152bb780dfc103b441e80625aae7b19
yoshua carrera vargas <yoshua.carrera24@gmail.com>
2026-08-08 23:05:09 -0500

    refactor ghostty config

--- modules/home-manager/apps/development/terminal/ghostty/default.nix
+++ modules/home-manager/apps/development/terminal/ghostty/default.nix
@@ -1,11 +1,12 @@
 {
-  pkgs,
   lib,
   config,
   ...
 }: {
   options = {ghostty.enable = lib.mkEnableOption "Enable ghostty module";};
   config = lib.mkIf config.homeManager.apps.development.terminal.ghostty.enable {
-    home.packages = with pkgs; [ghostty];
+    programs.ghostty = {
+      enable = true;
+    };
   };
 }
