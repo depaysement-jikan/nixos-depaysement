987ad50739a4f62011a2f72dd7321b9ff39a6237
depaysement-jikan <depaysement-jikan@gmail.com>
2026-09-08 14:29:15 -0500

    add pi dns

--- hosts/shinobu/default.nix
+++ hosts/shinobu/default.nix
@@ -76,11 +76,12 @@
   networking.networkmanager.enable = true;
   networking.firewall = {
     enable = true;
-    allowedTCPPorts = [4200 3000];
+    allowedTCPPorts = [80 443 4200 3000];
     trustedInterfaces = ["cni0" "flannel.1"];
   };
   networking.networkmanager.dns = "none";
   networking.nameservers = [
+    "192.168.1.202"
     "1.1.1.1"
   ];
 
