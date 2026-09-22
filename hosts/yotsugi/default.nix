6eb73ea246957e415c5d829c06f56c63737fa05c
depaysement-jikan <depaysement-jikan@gmail.com>
2026-09-22 13:39:35 -0500

    kernel upgrade, and disabling olh

--- hosts/yotsugi/default.nix
+++ hosts/yotsugi/default.nix
@@ -40,6 +40,7 @@
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
 
   hardware.enableRedistributableFirmware = true;
   networking.hostName = "yotsugi";
